e0c148849c77ad9120bed21857c83fec420ceddd
roblot <f.roblot.coulanges@gmail.com>
2024-09-01 19:51:00 +0200

    création du panier

--- src/components/Destination.js
+++ src/components/Destination.js
@@ -3,6 +3,8 @@ import tokyo from "../assets/tokyo.jpg";
 import montreal from "../assets/montreal.jpg";
 import londres from "../assets/londres.jpg";
 import newyork from "../assets/new-york.jpg";
+import { useState } from 'react';
+import "../styles/destination.css";
 
 function Destination() {
   const voyages = [
@@ -13,26 +15,37 @@ function Destination() {
     { id: 5, name: 'Montréal', country: 'Canada', duree: 12, price: 930, description: 'Ville bilingue avec une culture riche', image: montreal },
   ];
 
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="container mt-5">
       <div className="row row-cols-1 row-cols-lg-2">
         {voyages.map((voyage) => (
           <div key={voyage.id} className="col mb-4">
-            {/* <a  className="text-decoration-none"> */}
-              <div className="card" >
-                <img src={voyage.image} className="card-img-top w-100 h-100" alt={voyage.name} />
-                <div className="card-body">
-                  <h5 className="card-title">{voyage.name}</h5>
-                  <p className="card-text">{voyage.description} - {voyage.country}</p>
-                  <small>{voyage.duree} jours - {voyage.price} €</small>
-                  <br />
-                  {/* <a href="#" className="btn btn-info mt-2">Découvrir</a> */}
-                </div>
+            <div className="card">
+              <img src={voyage.image} className="card-img-top w-100 h-100" alt={voyage.name} />
+              <div className="card-body">
+                <h5 className="card-title">{voyage.name}</h5>
+                <p className="card-text">{voyage.description} - {voyage.country}</p>
+                <small>{voyage.duree} jours - {voyage.price} €</small>
+                <br />
+                <button className="btn bouton" onClick={() => setTotalPrice(totalPrice + voyage.price)}>Ajouter au panier</button>
               </div>
-            {/* </a> */}
+            </div>
           </div>
         ))}
       </div>
+
+      {/* Bouton pour afficher/masquer le panier */}
+      {isOpen ? (
+        <div className="mt-4 panier">
+          <h3>Prix total: {totalPrice} €</h3>
+          <button className="btn bouton" onClick={() => setIsOpen(false)}>Fermer le panier</button>
+        </div>
+      ) : (
+        <button className="btn bouton"  onClick={() => setIsOpen(true)}>Mon panier</button>
+      )}
     </div>
   );
 }
