a8959f3a766b09f90ad7dd9e3f6bfa0b6b2b8eea
roblot <f.roblot.coulanges@gmail.com>
2024-09-06 16:14:02 +0200

    utilisation de useEffect dans mon panier

--- src/components/Panier.js
+++ src/components/Panier.js
@@ -1,13 +1,22 @@
-// Panier.js
 import "../styles/panier.css";
+import { useState, useEffect } from 'react';
 
 function Panier({ totalPrice, setTotalPrice, isOpen, setIsOpen, voyages, clearCart }) {
+  const [isClearing, setIsClearing] = useState(false);
+
+  useEffect(() => {
+    if (isClearing) {
+      alert(`Vous êtes sur le point de vider votre panier`);
+      setIsClearing(false); // Réinitialiser l'état pour ne pas afficher l'alerte à chaque fois qu ele panier est vider
+    }
+  }, [isClearing]);
+
   return (
     isOpen ? (
       <div className="mt-4 open-cart">
         <div className="cart-header">
           <p>
-            {voyages.length > 0 ? (
+            {voyages.length > 0 ? ( 
               voyages.map(travel => (
                 <span key={travel.id}>
                   {travel.name} - {travel.price} € x {travel.quantity}
@@ -19,12 +28,17 @@ function Panier({ totalPrice, setTotalPrice, isOpen, setIsOpen, voyages, clearCa
             )}
           </p>
         </div>
-        <h3>Prix total: {totalPrice} €</h3>
+        <h3>Prix total : {totalPrice} €</h3>
         <button className="btn btn-warning" onClick={() => setIsOpen(false)}>
           Fermer 
         </button>
-        {/* <br /> */}
-        <button className="btn btn-danger mx-1" onClick={clearCart}>
+        <button
+          className="btn btn-danger mx-1"
+          onClick={() => {
+            setIsClearing(true); // Déclencher l'état pour afficher l'alerte
+            clearCart(); // Appeler la fonction pour vider le panier
+          }}
+        >
           Vider <i className="bi bi-basket3 fs-4"></i>
         </button>
       </div>
