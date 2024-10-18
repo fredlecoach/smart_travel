7923f7edb0ceb4f294803be0f785d0ee3ef6ba11
roblot <f.roblot.coulanges@gmail.com>
2024-10-18 14:49:43 +0200

    modif restaurants

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,5 +1,10 @@
 import mer from '../assets/mer.jpg';
 import montagne from '../assets/montagne.jpg';
+import paysan from '../assets/paysan.jpg';
+import bistrot from '../assets/bistrot.jpg';
+import brunch from '../assets/brunch.jpg';
+import vegetarien from '../assets/vegetarien.jpg';
+import vignoble from '../assets/vignoble.jpg';
 
 const restaurants = [
   {
@@ -21,15 +26,65 @@ const restaurants = [
     phone: "01 23 45 67 89",
     email: "restaurant@montagne.com",
     stars: "🌟🌟🌟"
+  },
+  {
+    id: 2,
+    name: "Chez papi",
+    image: paysan,
+    address: "12 rue de pépé",
+    place: "Strasbourg",
+    phone: "03 45 67 89 01",
+    email: "paysan@chezpapi.com",
+    stars: "🌟🌟"
+  },
+  {
+    id: 2,
+    name: "La bonne franquette",
+    image: bistrot,
+    address: "12 rue de la bonne franquette",
+    place: "Rouen",
+    phone: "02 65 45 98 67",
+    email: "lbf@yahoo.fr",
+    stars: "🌟"
+  },
+  {
+    id: 2,
+    name: "Le pti dej'",
+    image: brunch,
+    address: "12 rue de la montagne",
+    place: "Paris",
+    phone: "01 23 45 67 89",
+    email: "restaurant@montagne.com",
+    stars: "🌟🌟"
+  },
+  {
+    id: 2,
+    name: "La jungle",
+    image: vegetarien,
+    address: "12 rue de la montagne",
+    place: "Paris",
+    phone: "01 23 45 67 89",
+    email: "restaurant@montagne.com",
+    stars: "🌟"
+  },
+  {
+    id: 2,
+    name: "Pinot noir",
+    image: vignoble,
+    address: "12 rue de la montagne",
+    place: "Paris",
+    phone: "01 23 45 67 89",
+    email: "restaurant@montagne.com",
+    stars: "🌟🌟🌟"
   }
 ];
 
 export default function RestaurantList() {
   return (
-    <div className="d-flex flex-wrap justify-content-center">
+    <div className="d-flex flex-wrap justify-content-evenly mt-5">
       {restaurants.map((resto) => (
-        <div className="card m-2" style={{ width: "18rem" }} key={resto.id}>
-          <img src={resto.image} alt={`${resto.name}`} className="card-img-top" />
+        <div className="card m-2 " style={{ width: "20rem" }} key={resto.id}>
+          <img src={resto.image} alt={`${resto.name}`} className="card-img-top object-fit-cover h-100" />
           <div className="card-body">
             <h5 className="card-title">{resto.name}</h5>
             <ul className="list-unstyled">
