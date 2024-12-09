730590544da6c3c49a675b6d148adf32cfe1d59b
roblot <f.roblot.coulanges@gmail.com>
2024-12-09 01:19:13 +0100

    modif styled(Link)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import '../styles/Footer.css'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
-const StyledLink = styled.Link`text-decoration: none; color: white;
+const StyledLink = styled(Link)`text-decoration: none; color: white;
   &:hover{
   color: red
   }
